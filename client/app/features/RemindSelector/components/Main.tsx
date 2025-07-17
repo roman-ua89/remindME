e5136c14a62d7bdf7dfc9e4e3014e6c06b1e8fe9
Roman Melnychenko <roman.melnychenko@inspera.no>
2025-07-17 20:36:36 +0300

    Prettier config

--- client/app/features/RemindSelector/components/Main.tsx
+++ client/app/features/RemindSelector/components/Main.tsx
@@ -1,21 +1,21 @@
 'use client';
 
 import React from 'react';
-import {getSingleItems, getListItems} from "@/app/features/RemindSelector/actions";
-import { useActionState, startTransition, useEffect } from "react";
-import {redirect} from "next/navigation";
-import {IListNoteItem, ListNoteItem} from "@/app/features/ListNote/types";
-import {ISingleNoteItem} from "@/app/features/SingleNote/types";
-import {useAppDispatch, useAppSelector} from "@/store/hooks";
-import {setSingleNotes} from "@/store/features/singleNote/singleNoteSlice";
-import {setListNotes} from "@/store/features/listNote/listNoteSlice";
-import {deleteListNoteById, deleteSingleNoteById} from "@/app/shared/actions";
-import {ListView} from "@/app/features/RemindSelector/components/ListView";
-import {SingleView} from "@/app/features/RemindSelector/components/SingleView";
+import { getSingleItems, getListItems } from '@/app/features/RemindSelector/actions';
+import { useActionState, startTransition, useEffect } from 'react';
+import { redirect } from 'next/navigation';
+import { IListNoteItem, ListNoteItem } from '@/app/features/ListNote/types';
+import { ISingleNoteItem } from '@/app/features/SingleNote/types';
+import { useAppDispatch, useAppSelector } from '@/store/hooks';
+import { setSingleNotes } from '@/store/features/singleNote/singleNoteSlice';
+import { setListNotes } from '@/store/features/listNote/listNoteSlice';
+import { deleteListNoteById, deleteSingleNoteById } from '@/app/shared/actions';
+import { ListView } from '@/app/features/RemindSelector/components/ListView';
+import { SingleView } from '@/app/features/RemindSelector/components/SingleView';
 
 export const Main = () => {
-    const singleNotes = useAppSelector(state => state.singleType.notes);
-    const listNotes = useAppSelector(state => state.listType.notes);
+    const singleNotes = useAppSelector((state) => state.singleType.notes);
+    const listNotes = useAppSelector((state) => state.listType.notes);
     const dispatch = useAppDispatch();
 
     const [singleFormState, singleAction, singleIsPending] = useActionState(getSingleItems, []);
@@ -28,7 +28,7 @@ export const Main = () => {
         startTransition(() => {
             singleAction();
             listAction();
-        })
+        });
     }, []);
 
     useEffect(() => {
@@ -39,65 +39,70 @@ export const Main = () => {
             dispatch(setListNotes(listFormState));
         }
         if (Array.isArray(afterDeleteListState) && afterDeleteListState.length) {
-            dispatch(setListNotes(afterDeleteListState))
+            dispatch(setListNotes(afterDeleteListState));
         }
         if (Array.isArray(afterDeleteSingleState) && afterDeleteSingleState.length) {
-            dispatch(setSingleNotes(afterDeleteSingleState))
+            dispatch(setSingleNotes(afterDeleteSingleState));
         }
     }, [singleFormState, listFormState, afterDeleteListState, afterDeleteSingleState]);
 
-    const editSingleActionHandler = (id: ISingleNoteItem["id"]) => {
+    const editSingleActionHandler = (id: ISingleNoteItem['id']) => {
         redirect(`/single/edit/${id}`);
+    };
 
-    }
-
-    const editListActionHandler = (id: ListNoteItem["id"]) => {
+    const editListActionHandler = (id: ListNoteItem['id']) => {
         redirect(`/list/edit/${id}`);
-    }
+    };
 
-    const deleteListActionHandler = async (id: ListNoteItem["id"], title: string) => {
+    const deleteListActionHandler = async (id: ListNoteItem['id'], title: string) => {
         const confirmResult = confirm(`Are you sure you want to delete '${title}'`);
         if (confirmResult) {
             startTransition(() => {
                 deleteListByIdAction(id);
-            })
+            });
         }
-    }
+    };
 
-    const deleteSingleActionHandler = async (id: ISingleNoteItem["id"], term: ISingleNoteItem["term"]) => {
+    const deleteSingleActionHandler = async (id: ISingleNoteItem['id'], term: ISingleNoteItem['term']) => {
         const confirmResult = confirm(`Are you sure you want to delete '${term}'`);
         if (confirmResult) {
             startTransition(() => {
                 deleteSingleByIdAction(id);
-            })
+            });
         }
-    }
+    };
 
-    const playListActionHandler = (id: IListNoteItem["id"]) => {
+    const playListActionHandler = (id: IListNoteItem['id']) => {
         redirect(`/list/play/${id}`);
-    }
+    };
 
-    const playSingleNoteHandler = (id: ISingleNoteItem["id"]) => {
+    const playSingleNoteHandler = (id: ISingleNoteItem['id']) => {
         redirect(`/single/play/${id}`);
-    }
+    };
 
     return (
         <>
-            {singleIsPending ? (<div>singleIsPending...</div>) : (
+            {singleIsPending ? (
+                <div>singleIsPending...</div>
+            ) : (
                 <SingleView
                     singleNotes={singleNotes}
                     playSingleNoteHandler={playSingleNoteHandler}
                     editSingleActionHandler={editSingleActionHandler}
-                    deleteSingleActionHandler={deleteSingleActionHandler} />
+                    deleteSingleActionHandler={deleteSingleActionHandler}
+                />
             )}
 
-            {listIsPending ? (<div>singleIsPending...</div>) : (
+            {listIsPending ? (
+                <div>singleIsPending...</div>
+            ) : (
                 <ListView
                     listNotes={listNotes}
                     editListActionHandler={editListActionHandler}
                     deleteListActionHandler={deleteListActionHandler}
-                    playListActionHandler={playListActionHandler} />
+                    playListActionHandler={playListActionHandler}
+                />
             )}
         </>
-    )
-}
+    );
+};
